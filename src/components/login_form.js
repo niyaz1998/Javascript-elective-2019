5e1a521d77d51744024a29f2ecc57046864e129f
cepuchino <niyaz1998@yandex.ru>
2019-02-18 15:36:35 +0300

    client-server communication

--- src/components/login_form.js
+++ src/components/login_form.js
@@ -1,8 +1,11 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import {withStyles} from "@material-ui/core";
+import axios from "axios";
 
+const nameId = "name";
+const passwordId = "password";
 
 const styles = theme => ({
     loginForm: {
@@ -14,30 +17,49 @@ const styles = theme => ({
     }
 });
 
+const logInFunc = () => {
+    const name = document.getElementById(nameId).value;
+    const password = document.getElementById(passwordId).value;
+
+    axios({
+        method: 'post',
+        url: '/login',
+        data: {
+            name: name,
+            password: password
+        }
+    }).then((response) => {
+        console.log(response.data['answer']);
+    }).catch((e) => {
+        console.log(e);
+    }).then(() => {
+
+    });
+};
+
 class LoginTab extends Component {
     render() {
         return (
             <div className={this.props.classes.loginForm}>
-                <form onSubmit={() => {}}>
-                    <TextField
-                        id="name"
-                        name="name"
-                        label="Name"
-                        fullWidth/>
-                    <TextField
-                        id="password"
-                        name="password"
-                        label="Password"
-                        fullWidth
-                        type="password"/>
-                    <Button className={this.props.classes.button}
+                <TextField
+                    id={nameId}
+                    name={nameId}
+                    label="Name"
+                    fullWidth/>
+                <TextField
+                    id={passwordId}
+                    name={passwordId}
+                    label="Password"
+                    fullWidth
+                    type="password"/>
+                <Button className={this.props.classes.button}
                         type="submit"
                         fullWidth
                         variant="raised"
-                        color="primary">
-                        Log In
-                    </Button>
-                </form>
+                        color="primary"
+                        onClick={logInFunc}>
+                    Log In
+                </Button>
             </div>
         );
     }

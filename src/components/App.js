6459911e18cc34e9d50a70c0835e088c466ab379
cepuchino <niyaz1998@yandex.ru>
2019-02-27 15:53:25 +0300

    added main routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
-import LoginTab from  "./login_form";
+import {BrowserRouter, Route} from 'react-router-dom'
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import orange from '@material-ui/core/colors/orange';
+
+import LoginTab from  "./login_form";
 
 const theme = createMuiTheme(
     {
@@ -18,7 +20,18 @@ class App extends Component {
     render() {
         return (
             <MuiThemeProvider theme={theme}>
-                <LoginTab />
+                <BrowserRouter>
+                    <div>
+                        // login page
+                        <Route path="/login" component={LoginTab}/>
+                        // page to see list of all excursions (short information)
+                        <Route path="/excursions" component={() => <h1> Not implemented yet</h1>}/>
+                        // page to add new excursion
+                        <Route path="/add_excursion" component={() => <h1> Not implemented yet</h1>}/>
+                        // page to see detailed information about particular excursions (by id) and edit fields if needed
+                        <Route path="/excursion/:id" component={() => <h1> Not implemented yet</h1>}/>
+                    </div>
+                </BrowserRouter>
             </MuiThemeProvider>
         );
     }

2eb46867b628d8fb68c5cfebc2e1feafceb989fb
cepuchino <niyaz1998@yandex.ru>
2019-03-23 18:03:44 +0300

    excursion item refactored

--- src/components/ExcursionListItem/excursionListItem.js
+++ src/components/ExcursionListItem/excursionListItem.js
@@ -7,31 +7,35 @@ import Button from '@material-ui/core/Button';
 
 import styles from './excursionListItem.css';
 
+
+/*
+Excursions key set:
+id, title, price, start_time, duration,
+services, image, type, images, description,
+starting_point, available_dates, price_adult, price_child
+ */
 export const ExcursionListItem = ({excursion}) => {
     return (
         <ListItem className={styles.item}>
             <Card className={styles.card}>
-                <img src={excursion['main_photo']} className={styles.image}/>
+                <img src={excursion['image']} className={styles.image} alt={"Loading..."}/>
                 <div className={styles.details}>
                     <CardContent>
                         <Typography component="h5" variant="h5">
-                            {excursion['title_original']}
+                            {excursion['title']}
                         </Typography>
                         <Typography>
-                            {excursion['start_time'] + ' - ' + excursion['end_time'] + ' длительность: ' + excursion['duration']}
+                            { 'Начало: ' + excursion['start_time'] + ' длительность: ' + excursion['duration']}
                         </Typography>
                         <Typography>
-                            {'Цена для взрослых: ' + excursion['adult_price'] + '. Цена для детей: ' + excursion['child_price'] }
-                        </Typography>
-                        <Typography>
-                            {'Язык: ' + excursion['language']}
+                            {'Цена для взрослых: ' + excursion['price_adult'] + ' РУБ. Цена для детей: ' + excursion['price_child'] + ' РУБ' }
                         </Typography>
                     </CardContent>
                     <CardActions className={styles.controls}>
                             <Button color="primary">
                                 редактировать
                             </Button>
-                            <Button variant="raised" color="primary">
+                            <Button variant="contained" color="primary">
                                 Удалить
                             </Button>
                     </CardActions>

0fff5b77662f9c39877c5cc224698025b1bea610
cepuchino <niyaz1998@yandex.ru>
2019-04-20 10:16:20 +0300

    stub server changes

--- stub/data/excursions_list.js
+++ stub/data/excursions_list.js
@@ -1,66 +1,56 @@
-const excursions_list = [
+const excursions_list = {
+    "status": 0,
+    "errorMessage": "",
+    "excursions": [
         {
             'id': 0,
+            'region': 'RU',
             'title': 'Экскурсия 1',
+            'price': 100.0,
             'start_time': '12:00',
-            'end_time': '13:00',
-            'duration': '1 час',
-            'price': '100 РУБ',
-            'services': 'HUMN/LINE/BOAT',
-            'main_photo': 'https://im0-tub-ru.yandex.net/i?id=e1db90c3be0001e0dfc6e1dce88b67a7&n=13&exp=1',
-            'title_original': 'Очень-очень-очень длинное название экскурсии',
+            'duration': 120,
+            'services': ["HUMN", "LINE", "BOAT"],
             'type': 'WTCH',
-            'language': 'РУС',
-            'description': 'Тут идёт какое-то описание экскурсии, оно очень развернутое, или не очень,' +
-                ' мне просто текст нужен',
-            'departure': 'ул. Отправки 12',
-            'destination': 'ул. Конечная 23',
-            'adult_price': '100 РУБ',
-            'child_price': '70 РУБ',
-            'dates':
-                [
-                    '28.02.2019',
-                    '01.03.2019',
-                    '08.03.2019',
-                    '15.03.2019',
-                ],
-            'images': [
+            "images": [
+                'https://im0-tub-ru.yandex.net/i?id=e1db90c3be0001e0dfc6e1dce88b67a7&n=13&exp=1',
                 'https://arduff.files.wordpress.com/2008/10/norway-013.jpg',
                 'https://im0-tub-ru.yandex.net/i?id=37bff7b27c408b19bf0ed29d82874fad&n=33&w=225&h=150',
-                'http://travelbelka.ru/wp-content/uploads/2016/09/ARHIZ-OBL-1068x712.jpg']
+                'http://travelbelka.ru/wp-content/uploads/2016/09/ARHIZ-OBL-1068x712.jpg'
+            ],
+            "description": "some description",
+            "starting_point": "some starting point",
+            "available_dates": [
+                1552642200000,
+                1552908600000
+            ],
+            "price_adult": 400.0,
+            "price_child": 200.0
         },
         {
             'id': 1,
             'title': 'Экскурсия 2',
+            'region': 'RU',
+            'price': 400.0,
             'start_time': '10:00',
-            'end_time': '14:00',
-            'duration': '4 час',
-            'price': '400 РУБ',
-            'services': 'TICT/FORK/BEDH',
-            'main_photo': 'http://mypresentation.ru/documents/2a6d4215f6af44701aa4d2b3cac0cf97/img38.jpg',
-            'title_original': 'Немного другое, но все ещё длинное название экскурсии',
-            'type': 'WAVE',
-            'language': 'РУС',
-            'description': 'Стихи:\n ' +
-                'Ничто не вечно под луною, завёл мой врач издалека\n' +
-                'Ведь мы не от хорошей жизни так с вами весело живём\n' +
-                'У женщин нету недостатков. А вот особенности - есть',
-            'departure': 'ул. Отправки 15',
-            'destination': 'ул. Конечная 32',
-            'adult_price': '400 РУБ',
-            'child_price': '200 РУБ',
-            'dates':
-                [
-                    '29.02.2019',
-                    '01.03.2019',
-                    '02.03.2019',
-                    '09.03.2019',
-                    '16.03.2019',
-                ],
-            'images': [
-                'http://radio1.news/files/image/05/26/88/lg!dou.jpg',
-                'https://im0-tub-ru.yandex.net/i?id=2daa20d0a05a8e2e5b92c5b98b666a10-l&n=13']
+            'duration': 240,
+            'services': ["FORK", "BEDH"],
+            'type': 'CULT',
+            "images": [
+                'https://arduff.files.wordpress.com/2008/10/norway-013.jpg',
+                'http://mypresentation.ru/documents/2a6d4215f6af44701aa4d2b3cac0cf97/img38.jpg',
+                'https://im0-tub-ru.yandex.net/i?id=37bff7b27c408b19bf0ed29d82874fad&n=33&w=225&h=150',
+                'http://travelbelka.ru/wp-content/uploads/2016/09/ARHIZ-OBL-1068x712.jpg'
+            ],
+            "description": "another description",
+            "starting_point": "another starting point",
+            "available_dates": [
+                1552682200000,
+                1552988600000
+            ],
+            "price_adult": 300.0,
+            "price_child": 230.0
         }
-    ];
+    ]
+};
 
 module.exports = excursions_list;
